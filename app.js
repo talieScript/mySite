ac1689ca05bd0e74901d231a94856c164d59411b
talieScript <taliesin.bowes@hotmail.co.uk>
2019-01-15 00:24:46 +0000

    i hope this works

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express"),
+    app       = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname+"/public"));
+
+app.get("/", function(req, res){
+    res.render("index")
+});
+
+app.listen(8000, () => {
+    console.log('Example app listening on port 8000!')
+  });
