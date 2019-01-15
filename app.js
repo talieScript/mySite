8fc0a581b1f8646be24dd333eef86e0d40c9691f
talieScript <taliesin.bowes@hotmail.co.uk>
2019-01-15 10:12:32 +0000

    fixing issues with images

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ app.get("/", function(req, res){
 });
 
 app.listen(process.env.PORT || 8000, () => {
-    console.log('Example app listening on port 8000!')
+    console.log('app listening on port 8000!')
   });
