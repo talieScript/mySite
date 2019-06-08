ecd69e17750ea21440982888ff87a1ffdd65c392
Taliesin <taliesin.bowes@hotmail.co.uk>
2019-06-08 23:40:46 +0100

    finnish About section layout

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express"),
     app       = express();
 
 app.use(express.static(__dirname+"/public"));
+// app.set("view engine", "ejs")
 
 app.get("/", function(req, res){
-    res.render("index.html")
+    res.render("index.html");
 });
 
 app.listen(process.env.PORT || 8000, () => {
