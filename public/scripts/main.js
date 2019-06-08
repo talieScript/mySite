16387bc5aa9224a0290b77701644637ee014864b
Taliesin <taliesin.bowes@hotmail.co.uk>
2019-06-08 11:59:41 +0100

    finnished header layout, still needs img

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,6 @@
 // make nav items clickable 
 let navItems = document.querySelectorAll('.nav__item');
 navItems.forEach((el) => {
-    let href = el.childNodes[0].getAttribute('href');
     el.addEventListener('click', () => {
         el.childNodes[0].click();
     });
