68ee1cdff4119c8afe324b3587b224d30a56946b
Taliesin <taliesin.bowes@hotmail.co.uk>
2019-06-10 21:47:39 +0100

    Made site fully responsive from 350px - 1600px+

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,4 +5,25 @@ navItems.forEach((el) => {
         el.childNodes[0].click();
     });
 });
+
+// colapsable navigation
+const navCheckbox = document.querySelector('#nav-checkbox');
+const navBar = document.querySelector('#nav');
+const navItem = document.querySelectorAll('.nav__item');
+const navIcon = document.querySelector('#nav-icon');
+navCheckbox.addEventListener('click', () => {
+    if(navCheckbox.checked === true ) {
+        navBar.classList.add('nav--animate');
+        navIcon.classList.add('nav__icon--animate');
+        navItem.forEach((el) => {
+            el.classList.add('nav__item--animate');
+        })
+    } else {
+        navBar.classList.remove('nav--animate');
+        navIcon.classList.remove('nav__icon--animate');
+        navItem.forEach((el) => {
+            el.classList.remove('nav__item--animate');
+        })
+    }
+});
     
